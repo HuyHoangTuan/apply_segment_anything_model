a6c47104a4991c3a71f09f4ea32cb92ce26df812
HuyHt <kubin2712@gmail.com>
2023-06-19 13:15:25 +0700

    Update context.tsx

--- src/components/hooks/context.tsx
+++ src/components/hooks/context.tsx
@@ -14,7 +14,7 @@ const AppContextProvider = (props: {
   const [clicks, setClicks] = useState<Array<modelInputProps> | null>(null);
   const [image, setImage] = useState<HTMLImageElement | null>(null);
   const [maskImg, setMaskImg] = useState<HTMLImageElement | null>(null);
-
+  console.log('test1 pass');
   return (
     <AppContext.Provider
       value={{
